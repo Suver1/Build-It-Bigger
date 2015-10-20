9f3e26354714a51e03b0c8fea251c40eae8b2b78
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-10-20 22:39:31 +0200

    Added Java library for providing jokes

---
+++ jokeprovider/src/main/java/no/ahoi/builditbigger/JokeProvider.java
@@ -0,0 +1,19 @@
+package no.ahoi.builditbigger;
+
+public class JokeProvider {
+    String mJoke01 = "A husband and wife are trying to set up a new password for their computer. " +
+            "The husband puts, \"Mypenis,\" and the wife falls on the ground laughing because on " +
+            "the screen it says, \"Error. Not long enough.\"";
+    String mJoke02 = "A teacher is teaching a class and she sees that Johnny isn't paying " +
+            "attention, so she asks him, \"If there are three ducks sitting on a fence, and " +
+            "you shoot one, how many are left?\" Johnny says, \"None.\" The teacher asks, " +
+            "\"Why?\" Johnny says, \"Because the shot scared them all off.\" The teacher " +
+            "says, \"No, two, but I like how you're thinking.\" Johnny asks the teacher, " +
+            "\"If you see three women walking out of an ice cream parlor, one is licking her " +
+            "ice cream, one is sucking her ice cream, and one is biting her ice cream, which " +
+            "one is married?\" The teacher says, \"The one sucking her ice cream.\" Johnny " +
+            "says, \"No, the one with the wedding ring, but I like how you're thinking!\"";
+    public String getJoke() {
+        return mJoke01;
+    }
+}
