9a16f4e4c1cbaefb4f7a78de8cc55098c83a69b3
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-10-23 22:04:38 +0200

    removed floating action button

--- jokeWiz/src/main/java/no/ahoi/gradle/jokewiz/JokeShow.java
+++ jokeWiz/src/main/java/no/ahoi/gradle/jokewiz/JokeShow.java
@@ -2,11 +2,8 @@ package no.ahoi.gradle.jokewiz;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -26,15 +23,6 @@ public class JokeShow extends AppCompatActivity {
             TextView textView = (TextView) findViewById(R.id.joke);
             textView.setText(extras.getString("joke"));
         }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
