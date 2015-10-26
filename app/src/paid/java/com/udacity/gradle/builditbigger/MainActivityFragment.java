74b8f50cfeeea491dfefd8b4a851459d34156360
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-10-26 20:41:27 +0100

    Added paid flavor

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,20 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivityFragment extends Fragment {
+    private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
